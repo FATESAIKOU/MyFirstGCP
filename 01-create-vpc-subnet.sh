c836b7ea2210180ed74e0148575a8e894456bd51
FATESAIKOU <qzsecftbhhhh@gmail.com>
2025-12-19 23:29:21 +0900

    Add private compute instance and router/nat

--- 01-create-vpc-subnet.sh
+++ 01-create-vpc-subnet.sh
@@ -3,5 +3,8 @@
 gcloud compute networks create "$VPC_NAME" --subnet-mode=custom
 gcloud compute networks list
 
-gcloud compute networks subnets create "$SUBNET_NAME" --network="$VPC_NAME" --range "$SUBNET_RANGE" # --region=asia-northeast1
+gcloud compute networks subnets create "$SUBNET_NAME" --network="$VPC_NAME" --range "$SUBNET_RANGE" # --enable-private-ip-google-access
 gcloud compute networks subnets list --network="$VPC_NAME"
+
+# gcloud compute routes create ROUTE_NAME --network=VPC_NAME --destination-range=DESTINATION_RANGE --next-hop-address=NEXT_HOP_ADDRESS
+# gcloud compute routes list
--- 03-create-computeinstance.sh
+++ 03-create-computeinstance.sh
@@ -24,9 +24,7 @@ gcloud compute instances create "$VM_NAME_SSHCLOSED" \
   --tags="$SERVICE_FIREWALL_RULE_NAME" \
   --boot-disk-size="20GB"
 
-
 # gcloud compute instances list
-
 # ⭕️ gcloud compute ssh "$VM_NAME_SSHOPEN"
 # ⭕️ gcloud compute ssh "$VM_NAME_SSHOPEN" --tunnel-through-iap
 # ❌ gcloud compute ssh "$VM_NAME_SSHIAP"
@@ -34,3 +32,17 @@ gcloud compute instances create "$VM_NAME_SSHCLOSED" \
 # ❌ gcloud compute ssh "$VM_NAME_SSHCLOSED"
 # ❌ gcloud compute ssh "$VM_NAME_SSHCLOSED" --tunnel-through-iap
 # (show ssh command) gcloud compute ssh "$VM_NAME" --dry-run
+
+# Create a VM without external IP
+gcloud compute instances create "$VM_NAME_SSHIAP_PRIVATE" \
+  --machine-type="$MACHINE_TYPE" \
+  --subnet="$SUBNET_NAME" \
+  --image-family="$IMAGE_FAMILY" \
+  --image-project="$IMAGE_PROJECT" \
+  --no-address \
+  --tags="$SSHIAP_FIREWALL_RULE_NAME,$SERVICE_FIREWALL_RULE_NAME" \
+  --boot-disk-size="20GB"
+
+# create router and nat for private VM to access internet
+gcloud compute routers create "$ROUTER_NAME" --network="$VPC_NAME"
+gcloud compute routers nats create "$NAT_NAME" --router="$ROUTER_NAME" --auto-allocate-nat-external-ips --nat-all-subnet-ip-ranges
